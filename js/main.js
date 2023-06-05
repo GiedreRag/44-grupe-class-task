9093fed5f627253f2a477194ee333da5c9371e0d
Giedre <giedre.rageliene@gmail.com>
2023-06-05 15:13:12 +0300

    Css and jsonData

--- js/main.js
+++ js/main.js
@@ -1,11 +1,19 @@
 const divDOM = document.querySelector('div');
 
-const data = [];
+const data = fetch('https://front-end-by-rimantas.github.io/44-grupe-portfolio/css/components/services.css')
+    .then((response) => {
+        return response.text();
+    })
+    .then((data) => {
+        for (const user of data) {
+            console.log(user);
+        }
+    });
 
-let HTML = '';
+// let HTML = '';
 
-for (const item of data) {
-    HTML += ``;
-}
+// for (const item of data) {
+//     HTML += ``;
+// }
 
-divDOM.innerHTML = HTML;
+// divDOM.innerHTML = HTML;
