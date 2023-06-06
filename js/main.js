a6a307691014a360a0d441adbcab109f82a8c7a9
Giedre <giedre.rageliene@gmail.com>
2023-06-06 09:10:56 +0300

    Js update

--- js/main.js
+++ js/main.js
@@ -1,18 +1,17 @@
 const servicesDOM = document.querySelector('.services');
 
-let HTML = '';
-
 const data = fetch('https://giedrerag.github.io/44-grupe-class-task/data/services.json')
     .then((response) => {
         return response.json();
     })
     .then((data) => {
+        let HTML = '';
         for (const services of data) {
             HTML += `<div class="service">
                         <i class="et-line icon-${services.icon}"></i>
                         <h3 class="service-title">${services.title}</h3>
                         <p class="service-description">${services.desc}</p>
                     </div>`;
-            servicesDOM.innerHTML = HTML
         }
+        servicesDOM.innerHTML = HTML
     });
